ffc061055cdf56bfcb68c0381e086693a90f7754
Hiacecommuter <guo55wei@gmail.com>
2022-12-15 16:10:30 +1100

    add content in doubleUnderscore.py

--- classAttributesVsParameters.py
+++ classAttributesVsParameters.py
@@ -1,3 +1,10 @@
 """
 class attribute vs parameters
 """
+
+"""
+In OOP, for example Python, there are two concepts in class -- attribute and parameter.
+Attributes are value for object instances. 
+For example, the student ID is an attribute when we create a student instance from student class. 
+While, parameters are values passed to class methods when the methods are called.
+"""
