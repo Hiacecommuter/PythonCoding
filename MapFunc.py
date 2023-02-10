e1a4fff9c411c7c49a8e5ed1fc55093ee523c2e3
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-02-10 12:59:50 +1100

    Create MapFunc.py

---
+++ MapFunc.py
@@ -0,0 +1,11 @@
+"""
+map function in Python
+
+map(func, iter) function executes a specifiied function for each item for an iterable.
+"""
+
+print(map(lambda a: a*2, (1,2,3,4))
+      
+print(map(lambda x, y: x+y, [1,2,3,4,5], [10, 20, 30, 40, 50]))
+      
+print(map(int, ['1', '2', '3']))
