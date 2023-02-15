4d1a150637cfcdd6e53424ad0d6ca8ce87063c5a
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-02-15 21:17:31 +1100

    Update Regex.py

--- Regex.py
+++ Regex.py
@@ -3,17 +3,24 @@ import re
 match = re.findall(Regex_Pattern, Test_String)
 
 """
+. any character \.
+
 The expression \d matches any digit [ - ].
 The expression \D matches any character that is not a digit.
+
 \s matches any whitespace character [ \r\n\t\f ].
 \S matches any non-white space character
+
 \w will match any word character. Word characters include alphanumeric characters (-, - and -) and underscores (_).
 \W matches any non-word character. Non-word characters include characters other than alphanumeric characters (-, - and -) and underscore (_).
+
 The ^ symbol matches the position at the start of a string. 
 The $ symbol matches the position at the end of a string.
+
 The character class [ ] matches only one out of several characters placed inside the square brackets.
 The negated character class [^] matches any character that is not in the square brackets.
 A hyphen (-) inside a character class specifies a range of characters where the left and right operands are the respective lower and upper bounds of the range
+
 The tool {x} will match exactly  repetitions of character/character class/groups \w{4} -> H_ck
   w{3} : It will match the character w exactly  times.
   [xyz]{5} : It will match the string of length  consisting of characters {x, y, z}. For example it will match xxxxx, xxxyy and xyxyz.
@@ -23,10 +30,12 @@ The * tool will match zero or more repetitions of character/character class/grou
   w* : It will match the character w  or more times.
   [xyz]* : It will match the characters x, y or z  or more times.
   \d* : It will match any digit  or more times.
+
 The + tool will match one or more repetitions of character/character class/group.
    w+ : It will match the character w  or more times.
   [xyz]+ : It will match the character x, y or z  or more times.
   \d+ : It will match any digit  or more times.
+
 The $ boundary matcher matches an occurrence of a character/character class/group
 \b assert position at a word boundary.
 Three different positions qualify for word boundaries :
@@ -35,6 +44,13 @@ Three different positions qualify for word boundaries :
     After the last character in the string, if the last character is a word character
 () capture the group
 (?:) not capture the group
+
+|
+  (Bob|Kevin|Stuart) will match either Bob or Kevin or Stuart.
+  ([a-f]|[A-F]) will match any of the following characters: a, b, c, d, e, f, A, B, C, D, E, or F.
+ 
+\number match the th group
+  r'^([a-z])(\w)(\s)(\W)(\d)(\D)([A-Z])([A-Za-z])([aeiouAEIOU])(\S)\1\2\3\4\5\6\7\8\9\10$'
 """
 Regex_Pattern = r"\d\d\D\d\d\D\d\d\d\d"	# Do not delete 'r'.
 
