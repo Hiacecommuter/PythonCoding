30e3169bdd3a293762e4cc0d6a380d7b4ccf0dbe
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-08 15:14:15 +1000

    Create tools.py

---
+++ iters/tools.py
@@ -0,0 +1,14 @@
+import itertools
+#The module standardizes a core set of fast, memory efficient tools that are useful by themselves or in combination.
+
+# infinite iterators
+# count(), cycle(), repeat()
+
+#Iterators terminating on the shortest input sequence:
+# chain(), compress(), dropwhile(), groupby(), ifilter()
+# ifilterfalse(), islice(), imap(), starmap(), tee(), takewhile(), izip(), izip_longest()
+
+#Combinatoric generators
+# product(), permutations(), combinations(), combinations_with_replacement()
+
+#https://docs.python.org/3.10/library/itertools.html#itertools.product
