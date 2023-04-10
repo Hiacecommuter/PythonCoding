4466e532114376ce391c0e9f496f643142a14e88
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-10 18:57:04 +1000

    Create checkStrings.py

---
+++ strings/checkStrings.py
@@ -0,0 +1,9 @@
+# Built-in string checking
+
+#https://docs.python.org/3.10/library/stdtypes.html#str.isupper
+
+str.isalnum() # This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+str.isalpha() # This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+str.isdigit() # This method checks if all the characters of a string are digits (0-9).
+str.islower() # This method checks if all the characters of a string are lowercase characters (a-z).
+str.isupper() # 
