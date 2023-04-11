94be869d2a1bfa269aaae7f1feb3eb158c7b857e
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-11 10:36:15 +1000

    Create initVScall.py

---
+++ classes/initVScall.py
@@ -0,0 +1,11 @@
+"""
+                   __init__()	                        	                          __call__()
+Same as a constructor, it initializes the values	                  It is used for a direct call using the object
+It is invoked automatically when an object is declared		          It is invoked automatically by a callable
+Called by an regular object		                  	                  Called by a callable object
+Example:                        	                                  Example:
+a=A(3) #statement 1                          	                      a=A(3) #statement 1
+a() #statement 2                         	                          a() #statement 2
+__init__() is called by statement 1	                        	      __call__() is called by statement 2
+
+"""
