80ac37d5b724c87cbde070429327c408c4c95688
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-14 22:29:11 +1000

    Create mainAndName.py

---
+++ mainAndName.py
@@ -0,0 +1,10 @@
+"""
+if __name__ == "__main__"
+"""
+"""
+It allows to execute code when the file runs as a script,
+but not when it is imported as a module
+
+In the top-level code environment, the value of __name__ is "__main__".
+In an imported module, the value of __name__ is the module’s name as a string.
+"""
