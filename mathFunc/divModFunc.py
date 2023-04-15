06b14e3cbb8fcfd2684cfa1d6d655e5987a031c0
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-15 14:27:50 +1000

    Create divModFunc.py

---
+++ mathFunc/divModFunc.py
@@ -0,0 +1,5 @@
+"""
+One of the built-in functions of Python is divmod, which takes two arguments  and  and returns a tuple containing the quotient of  first and then the remainder .
+"""
+divmod(177,10)
+#(17, 7)
