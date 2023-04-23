495fb7c43b3bbf31eceeff50cfeb6928c92519fd
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-23 21:21:07 +1000

    Create count_find.py

---
+++ strings/count_find.py
@@ -0,0 +1,13 @@
+"""
+string.count(substring, start=..., end=...)
+The count() method returns the number of occurrences of a substring in the given string.
+"""
+
+"""
+str.find(sub[, start[, end]] )
+The find() method returns the index of first occurrence of the substring (if found). If not found, it returns -1.
+"""
+
+"""
+str.rfind()
+"""
