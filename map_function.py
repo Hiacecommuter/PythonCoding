0bd041852fc3cbb84354b52ba3134941704eff5d
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-04-24 15:42:23 +1000

    Update and rename MapFunc.py to map_function.py

--- map_function.py
+++ map_function.py
@@ -2,6 +2,8 @@
 map function in Python
 
 map(func, iter) function executes a specifiied function for each item for an iterable.
+
+map() also operates lazily
 """
 
 print(map(lambda a: a*2, (1,2,3,4))
