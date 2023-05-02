da2766c329cb0d67160556ebf985402e78509282
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-05-02 21:24:13 +1000

    Create measure_function_time.py

---
+++ decrators/measure_function_time.py
@@ -0,0 +1,27 @@
+""" measure the time a function takes to execture
+"""
+import functools
+import time
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()    # 1 return the float value of time in seconds
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+@timer
+def waste_some_time(num_times):
+    for _ in range(num_times):
+        sum([i**2 for i in range(10000)])
+        
+"""
+ If you want to do more precise measurements of code, 
+ you should instead consider the timeit module in the standard library. 
+ It temporarily disables garbage collection and runs multiple trials to strip out noise from quick function calls.
+ """
