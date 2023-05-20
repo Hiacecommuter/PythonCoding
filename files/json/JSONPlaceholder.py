6cdd863e0febdf7a0c3ed8dc22bd1909e1b9bd3c
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-05-21 00:05:38 +1000

    Update JSONPlaceholder.py

--- files/json/JSONPlaceholder.py
+++ files/json/JSONPlaceholder.py
@@ -1,4 +1,6 @@
 # example with
 # https://jsonplaceholder.typicode.com/
+import json
+import requests
 response = requests.get("https://jsonplaceholder.typicode.com/users/1/todos")
 todos = json.loads(response.text)
